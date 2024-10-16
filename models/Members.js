aaea2bd1403fc01e69bfe79a7772c68962448069
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-10-16 15:08:45 +0545

    Members mongoose schema

--- models/Members.js
+++ models/Members.js
@@ -1,26 +1,83 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const MembersSchema = mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
     },
     lastName: {
-        type: String
-    },
-    email: {
-        type: String
-    },
-    phoneNumber: {
-        type: String
-    },
-    password:{
-        type:String
+        type: String,
     },
     address: {
-        type: String
+        type: String,
+    },
+    phoneNumber: {
+        type: String,
+    },
+    secondPhoneNumber: {
+        type: String,
+    },
+    email: {
+        type: String,
     },
     dob: {
-        type: Date
+        type: Date,
+    },
+    gender: {
+        type: String,
+    },
+    membershipOption: {
+        type: String,
+    },
+    membershipType: {
+        type: String,
+    },
+    membershipDate: {
+        type: Date,
+    },
+    membershipDuration: {
+        type: String,
+    },
+    membershipRenewDate: {
+        type: Date,
+    },
+    membershipExpireDate: {
+        type: Date,
+    },
+    paymentMethod: {
+        type: String,
+    },
+    discountAmmount: {
+        type: String,
+    },
+    discountReason: {
+        type: String,
+    },
+    discountCode: {
+        type: String,
+    },
+    admissionFee: {
+        type: String,
+    },
+    finalAmmount: {
+        type: String,
+    },
+    paidAmmount: {
+        type: String,
+    },
+    dueAmmount: {
+        type: String,
+    },
+    receiptNo: {
+        type: String,
+    },
+    referenceCode: {
+        type: String,
+    },
+    remark: {
+        type: String,
+    },
+    actionTaker: {
+        type: String,
     }
 })
 
