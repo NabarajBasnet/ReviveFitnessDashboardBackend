580b278596b143fbabfc0613fdee8d3409cdbba1
nabaraj basnet <nabarajbasnet2000@gmail.com>
2024-10-22 14:47:56 +0545

    Temporary member attendance schema created

---
+++ models/AttendanceHistory/Members/24HourHistory.js
@@ -0,0 +1,33 @@
+const getNextMidnight = () => {
+    const now = new Date();
+    const tomorrow = new Date(now);
+    tomorrow.setHours(24, 0, 0, 0);
+    return tomorrow;
+};
+
+const TemporaryMemberAttendanceSchema = new mongoose.Schema({
+    memberId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Member'
+    },
+    fullName: {
+        type: String,
+    },
+    membershipOption: {
+        type: String,
+    },
+    checkInTime: {
+        type: Date,
+        default: Date.now
+    },
+    expiration: {
+        type: Date,
+        default: getNextMidnight
+    }
+});
+
+TemporaryMemberAttendanceSchema.index({ expiration: 1 }, { expireAfterSeconds: 0 });
+
+const TemporaryMemberAttendance = mongoose.models.temporarymemberattendance || mongoose.model('temporarymemberattendance', TemporaryMemberAttendanceSchema);
+module.exports = TemporaryMemberAttendance;
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const connectDatabase = require('./config/db');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
 const cookieParser = require('cookie-parser')
+const corn = require('node-cron')
+const TemporaryMemberAttendance = require('./models/AttendanceHistory/Members/24HourHistory');
 
 
 const port = process.env.PORT || 5000;
@@ -24,6 +26,17 @@ app.options('/api/members', cors(corsOptions));
 app.options('/api/auth', cors(corsOptions));
 app.use('/api/validate-qr', cors(corsOptions), require('./routes/validate-qr'))
 app.options('/api/validate-qr', cors(corsOptions));
+
+
+
+// corn job
+corn.schedule('0 0 * * *', async () => {
+    try {
+        await TemporaryMemberAttendance.deleteMany({});
+    } catch (error) {
+        console.log('Error: ', error);
+    }
+})
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
